ba448cac4de72de6240b5b0724b1d94334e29402
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-16 20:00:01 +0000

    Creating branch for BioC 3.1 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/gaia@102593 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/load_markers.R
@@ -0,0 +1,34 @@
+`load_markers` <-
+function(marker_matrix){
+
+message("Loading Marker Informations");
+
+chromosomes <- sort(unique(marker_matrix[,2]));
+
+
+chromosome_marker_list <- list();
+
+# Check if in the file there is the fourth column for the end position of the probes; 
+end_position <- FALSE;
+if(ncol(marker_matrix)==4){
+	end_position <- TRUE;
+}
+
+for( i in as.numeric(chromosomes) ){
+	chr_ids <- which(marker_matrix[,2]==i);
+	tmp_matrix <- matrix(0,2,length(chr_ids));
+	tmp_matrix[1,] <- marker_matrix[chr_ids,3];
+	if(end_position){
+		tmp_matrix[2,] <- marker_matrix[chr_ids,4];
+	}else{
+		tmp_matrix[2,] <- tmp_matrix[1,];
+	}
+	chromosome_marker_list[[i]] <- tmp_matrix;
+	names(chromosome_marker_list)[[i]] <- i;
+	message(".", appendLF = FALSE);
+}
+message("\nDone");
+#names(chromosome_marker_list) <- chromosomes;
+return(chromosome_marker_list);
+}
+
